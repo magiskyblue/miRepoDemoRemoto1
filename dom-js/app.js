f1fbf043e12fdbcebff938976fbcad458055b1c5
Maggie <201120142@ucc.mx>
2026-03-03 23:52:18 -0600

    cambios

---
+++ dom-js/app.js
@@ -0,0 +1,5 @@
+const listaItems = document.getElementsByTagName("ul");
+const item_nuevo = document.createElement("li"); 
+item_nuevo.innerText="Elemento 5";
+item_nuevo.classList.add("items-color1");
+listaItems[0].append(item.nuevo);
